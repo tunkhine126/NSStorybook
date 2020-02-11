c56bfeaf8b1df5621731352ebf85d97feaed6bbd
Tun Khine <tunkhine126@gmail.com>
2020-02-11 16:14:55 -0500

    Revised Annual Donations Script

--- SCRIPTS/AnnualDonations.rb
+++ SCRIPTS/AnnualDonations.rb
@@ -1,6 +1,6 @@
 ## Working script to get unique donors and their total donations & investment dollars in a particular year 
 
-donor_ids_by_amount = User.joins("LEFT JOIN donations ON users.id=donations.donor_id").where("donations.donor_id IS NOT NULL AND donations.reference_id IS NOT NULL AND donations.refund_reference_id IS NULL AND donations.created_at BETWEEN '2019-01-01 00:00:00' AND '2019-12-31 23:59:59'").having("SUM(donations.donation_total_in_dollars) > 0").having("SUM(donations.investment_total_in_dollars) > 0").group("users.id").pluck(:id).uniq
+donor_ids_by_amount = User.joins("LEFT JOIN donations ON users.id=donations.donor_id").where("donations.donor_id IS NOT NULL AND donations.reference_id IS NOT NULL AND donations.refund_reference_id IS NULL AND donations.created_at BETWEEN '2019-01-01 00:00:00' AND '2019-12-31 23:59:59'").having("SUM(donations.donation_total_in_dollars) >= 0").having("SUM(donations.investment_total_in_dollars) >= 0").group("users.id").pluck(:id).uniq
 
 donor_ids = donor_ids_by_amount
 
